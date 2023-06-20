3480d623cb221ee94be507547a2e3e1102ff5e3c
Pawel <pawelserbyn@wp.pl>
2023-06-20 20:18:21 +0200

    add simple drawer and logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,29 @@
 addEventListener("DOMContentLoaded", event => {
   const alerts = document.querySelectorAll(".alert");
-    console.log(alerts);
   if (alerts) {
     alerts.forEach(alert => {
       const deley = setTimeout(() => {
         alert.classList.add("alert__hide");
       }, 2000);
+    });
+  }
+
+  const accountAddBtn = document.querySelector(".js-account-add");
+  if (accountAddBtn) {
+    const overlay = document.querySelector(".overlay");
+    const drawer = document.querySelector(".drawer");
+    const drawerBackBtn = drawer.querySelector(".drawer__back");
+
+    accountAddBtn.addEventListener("click", () => {
+      drawer.classList.add("drawer--active");
+      overlay.classList.add("overlay--active");
+      document.body.classList.add("no-scroll");
+    });
+
+    drawerBackBtn.addEventListener("click", () => {
+      drawer.classList.remove("drawer--active");
+      overlay.classList.remove("overlay--active");
+      document.body.classList.remove("no-scroll");
     });
   }
 });
