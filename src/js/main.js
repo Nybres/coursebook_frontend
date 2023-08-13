111a706173cbb6928f347dea4dc0a7a7e621e437
Pawel <pawelserbyn@wp.pl>
2023-08-13 11:45:51 +0200

    zmina wyswietlania zdjec i drobne poprawki

--- src/js/main.js
+++ src/js/main.js
@@ -1,73 +1,73 @@
 addEventListener("DOMContentLoaded", event => {
-    const alerts = document.querySelectorAll(".alert");
-    if (alerts) {
-      alerts.forEach(alert => {
-        const deley = setTimeout(() => {
-          alert.classList.add("alert__hide");
-        }, 2000);
-      });
+  const alerts = document.querySelectorAll(".alert");
+  if (alerts) {
+    alerts.forEach(alert => {
+      const deley = setTimeout(() => {
+        alert.classList.add("alert__hide");
+      }, 2000);
+    });
+  }
+
+  const backdropActivate = overlay => {
+    overlay.classList.add("overlay--active");
+    document.body.classList.add("no-scroll");
+  };
+
+  const backdropDeActivate = overlay => {
+    overlay.classList.remove("overlay--active");
+    document.body.classList.remove("no-scroll");
+  };
+
+  const drawer = document.querySelector(".drawer");
+  if (drawer) {
+    const overlay = document.querySelector(".overlay");
+    const accountAddBtn = document.querySelector(".jsAccountAdd");
+    const drawerBackBtn = drawer.querySelector(".drawer__back");
+
+    accountAddBtn.addEventListener("click", () => {
+      drawer.classList.add("drawer--active");
+      backdropActivate(overlay);
+    });
+
+    drawerBackBtn.addEventListener("click", () => {
+      drawer.classList.remove("drawer--active");
+      backdropDeActivate(overlay);
+    });
+  }
+
+  const handleShadowNavigation = () => {
+    if (window.scrollY > 148) {
+      navigationElement.classList.add("nav--shadow");
+    } else {
+      navigationElement.classList.remove("nav--shadow");
     }
-  
-    const backdropActivate = overlay => {
-      overlay.classList.add("overlay--active");
-      document.body.classList.add("no-scroll");
-    };
-  
-    const backdropDeActivate = overlay => {
-      overlay.classList.remove("overlay--active");
-      document.body.classList.remove("no-scroll");
-    };
-  
-    const drawer = document.querySelector(".drawer");
-    if (drawer) {
-      const overlay = document.querySelector(".overlay");
-      const accountAddBtn = document.querySelector(".jsAccountAdd");
-      const drawerBackBtn = drawer.querySelector(".drawer__back");
-  
-      accountAddBtn.addEventListener("click", () => {
-        drawer.classList.add("drawer--active");
-        backdropActivate(overlay);
-      });
-  
-      drawerBackBtn.addEventListener("click", () => {
-        drawer.classList.remove("drawer--active");
-        backdropDeActivate(overlay);
-      });
-    }
-  
-    const handleShadowNavigation = () => {
-      if (window.scrollY > 148) {
-        navigationElement.classList.add("nav--shadow");
-      } else {
-        navigationElement.classList.remove("nav--shadow");
-      }
-    };
-  
-    const navigationElement = document.querySelector(".nav");
-    if (navigationElement) {
-      window.addEventListener("scroll", handleShadowNavigation);
-    }
-  
-    const addImageToCourseBtn = document.querySelector(".jsImagesAdd");
-    if (addImageToCourseBtn) {
-      const ImagesContainerElement = document.querySelector(".jsImagesCourse");
-      console.log(ImagesContainerElement);
-      addImageToCourseBtn.addEventListener("click", e => {
-        e.preventDefault();
-        const input = document.createElement("input");
-        input.classList.add("form__group-input");
-        input.type = "file";
-        input.name = "images";
-        input.accept = "image/*";
-        ImagesContainerElement.insertBefore(
-          input,
-          ImagesContainerElement.lastChild
-        );
-      });
-    }
-  
-    const form = document.querySelector(".jsCourseForm");
-  
+  };
+
+  const navigationElement = document.querySelector(".nav");
+  if (navigationElement) {
+    window.addEventListener("scroll", handleShadowNavigation);
+  }
+
+  const addImageToCourseBtn = document.querySelector(".jsImagesAdd");
+  if (addImageToCourseBtn) {
+    const ImagesContainerElement = document.querySelector(".jsImagesCourse");
+    console.log(ImagesContainerElement);
+    addImageToCourseBtn.addEventListener("click", e => {
+      e.preventDefault();
+      const input = document.createElement("input");
+      input.classList.add("form__group-input");
+      input.type = "file";
+      input.name = "images";
+      input.accept = "image/*";
+      ImagesContainerElement.insertBefore(
+        input,
+        ImagesContainerElement.lastChild
+      );
+    });
+  }
+
+  const form = document.querySelector(".jsCourseForm");
+  if (form) {
     form.addEventListener("submit", e => {
       e.preventDefault();
       const formData = new FormData(form);
@@ -79,9 +79,9 @@ addEventListener("DOMContentLoaded", event => {
           formData.append("uploaded_images", file);
         }
       });
-  
+
       const csrfToken = formData.get("csrfmiddlewaretoken");
-  
+
       fetch("/account-courses", {
         method: "POST",
         headers: {
@@ -101,5 +101,5 @@ addEventListener("DOMContentLoaded", event => {
           console.log(error);
         });
     });
-  });
-  
+  }
+});
